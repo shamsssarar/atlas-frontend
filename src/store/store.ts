66858785581c32a9b7fc6ae3310c258ff6f9c384
shamsssarar <shamsssarar@gmail.com>
2026-05-10 12:35:41 +0600

    feat: added storeProvider and wrapped the children with it

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { baseApi } from './baseApi';
 import authReducer from '@/features/auth/authSlice';
 
-export const store = configureStore({
-  reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
-});
+// Wrap configureStore in a function
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      [baseApi.reducerPath]: baseApi.reducer,
+      auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(baseApi.middleware),
+  });
+};
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+// Update the types to infer from the makeStore function
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
